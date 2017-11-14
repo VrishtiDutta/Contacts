b99aae0cd498928474b3e0ff874ff66f47740a93
Vrishti Dutta <Vrishti.Dutta@soti.net>
2017-11-14 09:54:17 -0500

    Base setup

--- Contacts/Program.cs
+++ Contacts/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Contacts
 {
+    class Contact
+    {
+        //Represents whether the contact is personal or business contact
+        private bool _isPersonal;
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String PhoneNumber { get; set; }
+
+        public Contact(bool personalContact)
+        {
+            _isPersonal = personalContact;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var person1 = new Contact(true)
+            {
+                FirstName = "Maya",
+                LastName = "Roy",
+                PhoneNumber = "6470040004"
+            };
+
+            List<Contact> allContacts = new List<Contact>();
+            allContacts.Add(person1);
         }
     }
 }
